318aa8a2d92725bf85842a270d23828d3f37069d
James Rae <jrae151515@yahoo.co.uk>
2023-01-15 16:29:56 +0000

    Added devise gem, also added  basic user & post models along with matching controllers, basic views to get core funtionality of these models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+  root "posts#index"
 end
