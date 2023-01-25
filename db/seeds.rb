2ec53d5f85357fcc9d8149912f17f7cb1868eefd
James Rae <jrae151515@yahoo.co.uk>
2023-01-25 19:41:06 +0000

    finished basic firend request feature, small bug fixes added here. Reset database to see with 4 new users to test with

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(:email => 'james@gmail.com', :password => 'password', :password_confirmation => 'password',
+    :first_name => 'James', :last_name => 'Rae')
+
+User.create!(:email => 'ocean@gmail.com', :password => 'password', :password_confirmation => 'password',
+    :first_name => 'Ocean', :last_name => 'Leigh')
+
+User.create!(:email => 'josh@gmail.com', :password => 'password', :password_confirmation => 'password',
+    :first_name => 'Josh', :last_name => 'Brown')
+
+User.create!(:email => 'guy@gmail.com', :password => 'password', :password_confirmation => 'password',
+    :first_name => 'Guy', :last_name => 'Geeson')
